0f95d52577b571342f46adae34fa1ddd75dced6a
LuizFernandesOliveira <luiz.fernandes@academico.ifrn.edu.br>
2020-08-08 17:09:38 -0300

    instalando axios

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, {useState} from 'react'
 import PageHeader from '../../components/PageHeader'
+import Input from '../../components/Input'
+
+import warningIcon from '../../assets/images/icons/warning.svg'
+
 import './styles.css'
+import Textarea from '../../components/Textarea'
+import Select from '../../components/Select'
 
 function TeacherForm(){
+
+    const [scheduleItems, setScheduleItems] = useState([
+        { week_day: 0, from: '', to: '' }
+    ])
+
+    function addNewScheduleItem(){
+        setScheduleItems([
+            ...scheduleItems,
+            { week_day: 0, from: '', to: '' }
+        ])
+    }
+
     return (
         <div id="page-teacher-form" className="container">
             <PageHeader 
@@ -13,21 +31,64 @@ function TeacherForm(){
             <main>
                 <fieldset>
                     <legend>Seus dados</legend>
-                    <div className="input-block">
-                        <label htmlFor="name">Nome Completo</label>
-                        <input type="text" id="name"/>
-                    </div>
-
-                    <div className="input-block">
-                        <label htmlFor="avatar">Avatar</label>
-                        <input type="text" id="avatar"/>
-                    </div>
-
-                    <div className="input-block">
-                        <label htmlFor="whatsapp">WhatsApp</label>
-                        <input type="text" id="whatsapp"/>
-                    </div>
+                    <Input name="name" label="Nome Completo"/>
+                    <Input name="avatar" label="Avatar"/>
+                    <Input name="whatsapp" label="WhatsApp" type="time"/>
+                    <Textarea name="bio" label="Biografia" />
                 </fieldset>
+                <fieldset>
+                    <legend>Sbre a aula</legend>
+                    <Select 
+                        name="subject" 
+                        label="Matéria"
+                        options={[
+                            {value: 'Artes', label: 'Artes'},
+                            {value: 'Biologia', label: 'Biologia'},
+                            {value: 'Matemática', label: 'Matemática'}
+                        ]}
+                    />
+                    <Input name="cost" label="Custo da sua aula por hora"/>
+                </fieldset>
+
+                <fieldset>
+                    <legend>
+                        Horários disponíveis
+                        <button type="button" onClick={addNewScheduleItem}>+ novo horário</button>
+                    </legend>
+                    {scheduleItems.map(scheduleItem => {
+                        return (
+                            <div key={scheduleItem.week_day} className="schedule-item">
+                            <Select 
+                                name="week_day" 
+                                label="Dia da Semana"
+                                options={[
+                                    {value: '0', label: 'Domingo'},
+                                    {value: '1', label: 'Segunda-feira'},
+                                    {value: '2', label: 'Terça-feira'},
+                                    {value: '3', label: 'Quarta-feira'},
+                                    {value: '4', label: 'Quinta-feira'},
+                                    {value: '5', label: 'Sexta-feira'},
+                                    {value: '6', label: 'Sabado'},
+                                ]}
+                            />
+                            <Input name="from" label="Das" type="time" />
+                            <Input name="to" label="Até" type="time" />
+                            </div>
+                        )
+                    })}
+                    
+                </fieldset>
+
+                <footer>
+                    <p>
+                        <img src={warningIcon} alt="Aviso importante" />
+                        Importante! <br />
+                        Preencha todos os dados
+                    </p>
+                    <button type="submit">
+                        Salvar cadastro
+                    </button>
+                </footer>
             </main>
 
         </div>
